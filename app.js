a18c04b744f06f4e5adfd5c93d7716d0dae9aa7a
ADITYA MAURYA <adityamaurya@Mac.lan>
2026-06-07 21:17:39 +0530

    added root redirect

--- app.js
+++ app.js
@@ -90,6 +90,9 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 app.use("/listings", ListingRouter);
 app.use("/listings/:id", reviewRouter);
 app.use("/", userRouter);
